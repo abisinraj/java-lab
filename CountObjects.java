f9b06568a1bdeb42ff29f39076e96fdfcb5595b4
Abisin Raj <abisinr201@gmail.com>
2026-02-19 21:41:44 +0530

    feat: Add Java program to count the number of objects created.

---
+++ CountObjects.java
@@ -0,0 +1,23 @@
+//program13
+//write a java program to count the number of objects created
+public class CountObjects {
+    static int count = 0;
+
+    public CountObjects() {
+        count = count + 1;
+    }
+
+    public CountObjects(int i) {
+        count = count + i;
+    }
+
+    public static void main(String[] args) {
+        CountObjects obj1 = new CountObjects();
+        CountObjects obj2 = new CountObjects();
+        CountObjects obj3 = new CountObjects();
+        CountObjects obj4 = new CountObjects();
+        CountObjects obj5 = new CountObjects();
+
+        System.out.println("Number of objects: " + CountObjects.count);
+    }
+}
