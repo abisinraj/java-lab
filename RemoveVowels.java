346b35dc0cefe9fe4c7976997780ca3d9eadba5b
Abisin Raj <abisinr201@gmail.com>
2026-02-19 21:28:54 +0530

    feat: add `RemoveVowels` utility class for string vowel removal

---
+++ RemoveVowels.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RemoveVowels {
+    public static void main(String[] args) {
+        String str1, str2;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        str1 = scan.nextLine();
+
+        str2 = str1.replaceAll("[aeiouAEIOU]", "");
+
+        System.out.println(str2);
+
+        scan.close();
+    }
+}
