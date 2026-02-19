24c234524072d8fc4387a52134a84c8ce8a2b963
Abisin Raj <abisinr201@gmail.com>
2026-02-19 21:24:04 +0530

    feat: Add Java program to calculate the sum of digits and reverse a number.

---
+++ SumRevMain.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class SumRev {
+    int a, num = 0, sum = 0;
+
+    void sum(int num) {
+        do {
+            a = num % 10;
+            sum = sum + a;
+            num = num / 10;
+        } while (num > 0);
+
+        System.out.println("Sum of digits = " + sum);
+    }
+
+    void reverse(int num) {
+        do {
+            a = num % 10;
+            this.num = this.num * 10 + a;
+            num = num / 10;
+        } while (num > 0);
+
+        System.out.println("Reverse: " + this.num);
+    }
+}
+
+public class SumRevMain {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any number: ");
+        n = sc.nextInt();
+
+        SumRev obj = new SumRev();
+        obj.sum(n);
+        obj.reverse(n);
+
+        sc.close();
+    }
+}
