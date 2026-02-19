c550c76919af509571162df1cdfc516790adfcdd
Abisin Raj <abisinr201@gmail.com>
2026-02-19 21:14:29 +0530

    feat: Add Java program to sort an array and find its smallest, largest, and second largest elements.

---
+++ ArrayElements.java
@@ -0,0 +1,35 @@
+
+//program6
+//write a java program to print all the elements of an array
+import java.util.Scanner;
+
+public class ArrayElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, temp, n = 10;
+        int arr[] = new int[10];
+
+        System.out.print("Enter 10 elements: ");
+        for (i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Sorting the array using bubble sort
+        for (i = 0; i < n; i++) {
+            for (j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Smallest element = " + arr[0]);
+        System.out.println("Largest element = " + arr[n - 1]);
+        System.out.println("Second largest element = " + arr[n - 2]);
+
+        sc.close();
+    }
+}
